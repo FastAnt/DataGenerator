a8885511ebea4ef76a4719f7d6f30f09d3ced3b0
ant <Artemrukikruki@gmail.com>
2018-11-24 11:17:57 +0200

    first refactor interation

--- generator_full.py
+++ generator_full.py
@@ -7,13 +7,27 @@ import random
 from PIL import Image
 from os import listdir
 import os
+
+NUMBER_OF_REPEATS = 2
+
+MIN_SIZE = 30
+MAX_SIZE = 250
+
+
+#zone for generative
+MINIMAL_X = 0
+MINIMAL_Y = 0
+
+
 ia.seed(1)
 
+# counter for naming
 global_counter = 1
-for root_back, dirs_back, files_back in os.walk('./background_hight'):
+
+for root_back, dirs_back, files_back in os.walk('./backgrounds'):
     for file_back in files_back:
-        for i in range(1, 6):
-            for root, dirs, files in os.walk('./img1'):
+        for i in range(1, NUMBER_OF_REPEATS):
+            for root, dirs, files in os.walk('./targets'):
                 for file in files:
                     if not file.endswith('.png'):
                         continue
@@ -23,28 +37,25 @@ for root_back, dirs_back, files_back in os.walk('./background_hight'):
                     old_txt_file = open(old_txt_name)
                     line = old_txt_file.readline()
                     new_line = ''
-                    # for j in range(1, 1):
-                    #     is_need_skip = random.randint(0, 10)
-                    #     if is_need_skip  > 4 :
                     foreground = Image.open(os.path.join(root, file))
                     orig_width = foreground.width
                     orig_height = foreground.height
 
-                    foreg_size = random.randint(200, 500)
+                    foreg_size = random.randint(MIN_SIZE, MAX_SIZE)
                     size_ratio_w = int(foreg_size) / foreground.width
                     size_ratio_h = int(foreg_size) / foreground.height
+
                     foreground = foreground.resize((int(foreg_size), int(foreg_size)), Image.ANTIALIAS)
                     if(background.width - foreground.width < 1):
                         continue
                     if (background.height - foreground.height < 1):
                         continue
-                    foreg_x = random.randint(0, background.width - foreground.width)
-                    foreg_y = random.randint(0, background.height - foreground.height)
+
+                    #paste target on background
+                    foreg_x = random.randint(int(background.width/2.5), background.width - foreground.width)
+                    foreg_y = random.randint(0, int((background.height - foreground.height)/2))
                     background.paste(foreground, (foreg_x, foreg_y), foreground)
 
-
-
-                    #print(old_txt_name)
                     new_line_list = line.split(' ')
                     if not new_line_list:
                         continue
@@ -60,8 +71,8 @@ for root_back, dirs_back, files_back in os.walk('./background_hight'):
                     new_line = str(new_line_list[0]) + ' ' + str(new_line_list[1]) + ' ' + str(new_line_list[2]) + ' ' + str(
                         new_line_list[3]) + ' ' + str(new_line_list[4]) + '\n'
 
-                    new_file_name_txt = 'result_directory_finger/' + str(global_counter)+'.txt'
-                    new_file_name_img = 'result_directory_finger/' + str(global_counter) + '.jpg'
+                    new_file_name_txt = 'result_directory/' + str(global_counter)+'.txt'
+                    new_file_name_img = 'result_directory/' + str(global_counter) + '.jpg'
                     new_file = open(new_file_name_txt, 'w')
                     new_file.write(new_line)
                     new_file.close()
